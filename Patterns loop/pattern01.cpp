ded372256c679cd3550237ee64cb5f30f37b4326
sabnam <sabnamhazari@gmail.com>
2024-01-29 23:17:31 +0530

    practice

---
+++ Patterns loop/pattern01.cpp
@@ -0,0 +1,25 @@
+
+//  Pattern 01
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
