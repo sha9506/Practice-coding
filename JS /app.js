d44996750152665aa6de739731f236b96244c1c0
sabnam <sabnamhazari@gmail.com>
2024-01-31 22:45:55 +0530

    js

---
+++ JS /app.js
@@ -0,0 +1,23 @@
+//let and constant 
+// let is used instead of var to initialize variable 
+let myname = "shabnam";
+myname="afsana";
+console.log(myname);
+
+//constant is used for a constant variable
+const myrealname ="shabnam"
+
+//ARROW FUNCTION
+
+//normal syntax
+function sayName(name)
+{
+    console.log(name);
+}
+sayName('afsana');
+
+let realName= names => names; //names is returned
+console.log(realName("afsana"));
+
+//EXPORT and IMPORT (modules)
+
